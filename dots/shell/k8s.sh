638115ae47f835341915a2c9687bc2ef5e9efbb2
bradfordwagner <wagner.bradford@gmail.com>
2023-02-15 14:52:02 -0500

    setup split

--- dots/shell/k8s.sh
+++ dots/shell/k8s.sh
@@ -39,8 +39,6 @@ alias k=kubectl
 alias s=switch # used to help with switching contexts and namespaces
 alias sni="switch --no-index" # do not use cached values
 alias snsk="s ns; k9"
-alias kc=kubectx
-alias kcc="kubectx -c"
 alias kn="kubens"
 alias kna="kn astra"
 alias knc="kubens -c"
@@ -59,31 +57,6 @@ alias sk="s ns; k9"
 alias k9a='k9s -r 1 -A'
 alias kk="k9"
 alias ka="k9a"
-function kcp() {
-  # file=$(mktemp /tmp/kubecontext.XXXX)
-  # echo file=${file}
-  # ll ${file}
-
-  KUBECONFIG=~/.kube/config
-
-  ctx_name=$(k config current-context)
-  # file=$(mktemp /tmp/${ctx_name}.XXXXX)
-  file=$(mktemp /tmp/${ctx_name}.XXXXX)
-  cp -L ${KUBECONFIG} ${file} # follow the link
-  export KUBECONFIG=${file}
-
-  # s ns
-
-  # check ks for tmuxSendToPane
-
-
-  # cp -L ~/.kube/config ${file}
-  # ll ${file}
-  # bat -P -lyaml ${file}
-  # ls -lh ${file}
-  # rm ${file}
-  # ll ${file}
-}
 function kaf() {
    arr=("$@")
    for i in "${arr[@]}"; do
@@ -96,5 +69,66 @@ function kdf() {
      kubectl delete -f ${i}
    done
 }
-alias kwai='clear; echo kube_config=$KUBECONFIG; ll ~/.kube/config; kcc; knc; hla'
+alias kwai='clear; echo kube_config=$KUBECONFIG; ll ~/.kube/config; kubectx -c; knc; hla'
 ################################################
+
+## smart completions - kc ######################
+################################################
+export kc_tmp_dir=/tmp/kc
+function kc() {
+  clear
+  items=(
+    context
+  )
+  choice=$(printf "%s\n" "${items[@]}" | fzf)
+  kc_app_${choice}
+}
+function kc_app_context() {
+  items=(
+    cp
+    clean
+  )
+  choice=$(printf "%s\n" "${items[@]}" | fzf)
+  kc_app_context_${choice}
+}
+function kc_app_context_clean() {
+  echo deleting ${kc_tmp_dir} files:
+  ll ${kc_tmp_dir}
+  rm -f ${kc_tmp_dir}/*
+}
+function kc_app_context_cp() {
+  local split=${1}
+  original_kubeconfig=${KUBECONFIG}
+  resolved_kubeconfig=${original_kubeconfig:-~/.kube/config}
+  mkdir -p ${kc_tmp_dir}
+
+  ctx_name=$(k config current-context)
+  file=$(mktemp ${kc_tmp_dir}/${ctx_name}.XXXXX)
+  cp -L ${resolved_kubeconfig} ${file} # follow the link
+  export KUBECONFIG=${file}   # update kubeconfig to the temporary file
+
+  # prompt for a split if not provided as an arg
+  items=(
+    -v
+    -h
+    inplace
+  )
+  [[ "${split}" == "" ]] && split=$(printf "%s\n" "${items[@]}" | fzf  --prompt 'tmux split: ')
+
+  if [[ "inplace" == "${split}" ]]; then
+    echo in place!
+    export KUBECONFIG=${file}
+    # s ns
+  else
+    pane_index=$(tmux splitw ${split} -P -F "#{pane_index}")
+    echo pane_index=${pane_index}
+    tmux send -t ${pane_index} "export KUBECONFIG=${file}" ENTER
+    tmux send -t ${pane_index} "s ns" ENTER
+
+    # reset to original kubeconfig
+    export KUBECONFIG=${original_kubeconfig}
+  fi
+
+  echo KUBECONFIG=${KUBECONFIG}
+}
+
