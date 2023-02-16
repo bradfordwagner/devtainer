637751a4732ea6b7fc08e12d732bff6e495f6242
bradfordwagner <wagner.bradford@gmail.com>
2023-02-15 23:51:39 -0500

    bugfix paste buffer, don't pate when you hit escape!

--- dots/shell_scripts/tmux_buffers.sh
+++ dots/shell_scripts/tmux_buffers.sh
@@ -2,5 +2,5 @@
 set -e
 
 buffer=$(tmux list-buffers -F "#{buffer_name}: #{buffer_sample}" 2>/dev/null | fzf --exit-0 --preview "echo {} | sed 's/.*://' | bat -f --style=full -l bash" | sed 's/:.*//')
-tmux paste-buffer -b ${buffer}
+[ "${buffer}" != "" ] && tmux paste-buffer -b ${buffer}
 
