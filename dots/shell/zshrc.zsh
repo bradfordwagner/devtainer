64814eab6e56f78d39a4031aaef445b0c1e4b7ce
bradfordwagner <wagner.bradford@gmail.com>
2023-03-02 11:45:47 -0500

    always show kube context

--- dots/shell/zshrc.zsh
+++ dots/shell/zshrc.zsh
@@ -32,6 +32,7 @@ setopt HIST_SAVE_NO_DUPS
 # environment loads
 # if [[ $(hostname -s) = nycla* ]] || [[ $(hostname -s) = NYCLA* ]] || [[ $(hostname -s) = macbook-pro ]] || [[ $(hostname -s) = nycmd* ]] || [[ $(hostname -s) = bwagner-* ]]; then
   export ZSH_THEME="powerlevel10k/powerlevel10k"
+  unset POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND # always show kube context
 # else
 #   # setup hail mary color scheme on remote
 #   export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
