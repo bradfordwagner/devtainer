2f55d47b824c941ec65d191e10dced099c3abf0e
bradfordwagner <wagner.bradford@gmail.com>
2023-04-12 09:20:21 -0400

    cleanup

--- dots/shell/terraform.zsh
+++ dots/shell/terraform.zsh
@@ -12,10 +12,3 @@ function tg_prune() {
   find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
 }
 
-# terraform completions
-# from: https://www.terraform.io/docs/cli/commands/index.html#shell-tab-completion
-# these don't work all that well just yet - v0.14.3
-#if [ -f /usr/local/bin/terraform ]; then
-#  autoload -U +X bashcompinit && bashcompinit
-#  complete -o nospace -C /usr/local/bin/terraform terraform
-#fi
