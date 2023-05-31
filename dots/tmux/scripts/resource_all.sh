eb44e8ab600296fb5d3c0e59740c8adf15824c7b
bradfordwagner <wagner.bradford@gmail.com>
2023-05-31 07:19:09 -0400

    got it working

--- dots/tmux/scripts/resource_all.sh
+++ dots/tmux/scripts/resource_all.sh
@@ -1,18 +1,25 @@
 set -x
 
-set_keytable=~/.tmux/conf/scripts/set_keytable.sh
-${set_keytable} insert
-
 sleep_duration=1
+# use actual names not alias'd names ie, base=b, where we use 'b' to set the layer
+set_keytable() {
+  local -r key_table=${1}
+  tmux set -t % key-table ${key_table}
+}
+
+set_keytable root
+
 
 tmux setw synchronize-panes
 tmux send C-z               # put processes in background
-sleep ${sleep_duration}     # wait a moment for things to reset
+sleep ${sleep_duration}
 tmux send C-c C-c           # cancel in case nothing was running
-sleep ${sleep_duration}     # wait a moment for things to reset
+sleep ${sleep_duration}
 tmux send resource Enter fg # source configs again
-sleep ${sleep_duration}     # wait a moment for things to reset
+sleep ${sleep_duration}
 tmux send Enter
-tmux setw synchronize-panes
 
-${set_keytable} base
+sleep ${sleep_duration}
+set_keytable b
+
+tmux setw synchronize-panes
