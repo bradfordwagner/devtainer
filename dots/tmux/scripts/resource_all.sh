9fee3f02ad63f314c1c3e96bdf47957f55d9af8f
bradfordwagner <wagner.bradford@gmail.com>
2023-05-31 07:05:56 -0400

    move back to base layer

--- dots/tmux/scripts/resource_all.sh
+++ dots/tmux/scripts/resource_all.sh
@@ -3,14 +3,16 @@ set -x
 set_keytable=~/.tmux/conf/scripts/set_keytable.sh
 ${set_keytable} insert
 
-sleep_duration=2
+sleep_duration=1
 
 tmux setw synchronize-panes
-tmux send C-Z
-sleep ${sleep_duration}  # wait a moment for things to reset
-tmux send resource Enter # source configs again
-tmux send fg             # bring processes to fg again
-sleep ${sleep_duration}  # wait a moment for things to reset
+tmux send C-z               # put processes in background
+sleep ${sleep_duration}     # wait a moment for things to reset
+tmux send C-c C-c           # cancel in case nothing was running
+sleep ${sleep_duration}     # wait a moment for things to reset
+tmux send resource Enter fg # source configs again
+sleep ${sleep_duration}     # wait a moment for things to reset
 tmux send Enter
 tmux setw synchronize-panes
 
+${set_keytable} base
