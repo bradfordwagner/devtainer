1f7cb295372cc66222510e4c95998475e18a54fb
bradfordwagner <wagner.bradford@gmail.com>
2023-06-30 23:32:06 -0400

    ctrl-d to delete buffer

--- dots/shell_scripts/tmux_buffers.sh
+++ dots/shell_scripts/tmux_buffers.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 set -e
 
-buffer=$(tmux list-buffers -F "#{buffer_name}: #{buffer_sample}" 2>/dev/null | fzf --exit-0 --preview "echo {} | sed 's/.*://' | bat -f --style=full -l bash" | sed 's/:.*//')
+# ctrl-d to delete buffer and refresh the buffer list
+buffer=$(tmux list-buffers -F "#{buffer_name}: #{buffer_sample}" 2>/dev/null \
+    | fzf --exit-0 --preview "echo {} | sed 's/.*://' | bat -f --style=full -l bash" \
+    --bind "ctrl-d:execute-silent(echo '{}' | sed 's/:.*//' | tmux deleteb -b)+reload(tmux list-buffers -F \"#{buffer_name}: #{buffer_sample}\")" \
+    | sed 's/:.*//'
+)
 [ "${buffer}" != "" ] && tmux paste-buffer -b ${buffer}
-
