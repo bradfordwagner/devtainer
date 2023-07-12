8cd67169ff377633a27959a35dda1bd3162266c4
bradfordwagner <wagner.bradford@gmail.com>
2023-07-12 07:45:59 -0400

    add sudo_fkill

--- dots/shell/alias.zsh
+++ dots/shell/alias.zsh
@@ -113,6 +113,16 @@ fkill() {
     echo $pid | xargs kill -${1:-9}
   fi
 }
+# sudo fkill - kill process
+sudo_fkill() {
+  local pid
+  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs sudo kill -${1:-9}
+  fi
+}
 
 ################################################
 # clipboard
