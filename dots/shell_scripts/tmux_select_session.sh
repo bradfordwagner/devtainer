40b14225814a3731ce7e4f69228849a5299e430e
bradfordwagner <wagner.bradford@gmail.com>
2023-07-19 05:57:33 -0400

    add preview to select session

--- dots/shell_scripts/tmux_select_session.sh
+++ dots/shell_scripts/tmux_select_session.sh
@@ -2,7 +2,8 @@
 set -e
 
 # https://github.com/junegunn/fzf/wiki/examples#tmux
-session=$(tmux list-sessions -F '#{session_name}' 2>/dev/null | fzf --exit-0)
-echo session=${session}
+session=$(tmux list-sessions -F '#{session_id}:#{session_name}' 2>/dev/null \
+  | fzf --exit-0 --delimiter=":" --with-nth=2 --preview="tmux list-windows -t {1}"
+)
 tmux switch-client -t ${session}
 
