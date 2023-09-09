90b0e56c4ee14e47e9e984c7c9be73a0acee6bc5
bradfordwagner <wagner.bradford@gmail.com>
2023-09-09 01:54:15 -0400

    re-order

--- dots/config/nvim/lua/bradfordwagner/init.lua
+++ dots/config/nvim/lua/bradfordwagner/init.lua
@@ -1,7 +1,9 @@
 -- load plugin managers
 require 'bradfordwagner.packer'
 
+-- must be before theme load
+require 'bradfordwagner.ui'
+
 -- second state
 require 'bradfordwagner.keybindings'
 require 'bradfordwagner.theme'
-require 'bradfordwagner.ui'
