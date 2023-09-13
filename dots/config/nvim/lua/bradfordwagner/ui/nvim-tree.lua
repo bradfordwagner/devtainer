4eebc39dd7a015505b869366375ba4b341a009c3
bradfordwagner <wagner.bradford@gmail.com>
2023-09-13 16:57:57 -0400

    do not allow nvim tree to hijack dirs

--- dots/config/nvim/lua/bradfordwagner/ui/nvim-tree.lua
+++ dots/config/nvim/lua/bradfordwagner/ui/nvim-tree.lua
@@ -3,5 +3,11 @@ require "nvim-tree".setup {
     float = {
       enable = false,
     }
-  }
+  },
+  -- do not allow nvim tree to open by default when a directory is opened
+  -- this especially affects jumpdir
+  hijack_directories = {
+    enable = false,
+    auto_open = true,
+  },
 }
