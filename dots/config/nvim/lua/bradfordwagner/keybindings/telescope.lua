0044203a559d9f94ebeb14dec98672eaf638b42e
bradfordwagner <wagner.bradford@gmail.com>
2023-09-16 00:56:01 -0400

    cleanup

--- dots/config/nvim/lua/bradfordwagner/keybindings/telescope.lua
+++ dots/config/nvim/lua/bradfordwagner/keybindings/telescope.lua
@@ -20,15 +20,7 @@ wk.register({
   -- git
   ['<space>dgl'] = { builtin.git_files, 'list files' },
   -- ag
-  ['<space>da'] = { name = 'Ag Silver Searcher' },
-  ['<space>daa'] = { '<cmd>Ag<cr>', 'live search' },
-  ['<space>dai'] = { function ()
-   vim.ui.input({ prompt = 'Ag> ' }, function (input)
-     if input ~= nil then vim.cmd(string.format('Ag %s', input)) end
-   end)
-  end,  'find input' },
-  ['<space>daw'] = { 'find word' },
-  ['<space>daW'] = { 'find WORD' },
+  ['<space>da'] = { '<cmd>Easypick ag<cr>', 'Ag Silver Searcher' },
   -- misc
   ['<space>dc'] = { builtin.commands, '' },
   ['<space>dm'] = { builtin.keymaps, '' },
