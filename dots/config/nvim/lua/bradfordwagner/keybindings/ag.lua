a2c7372645b307892dc4a5cdadb50181123f8c4b
bradfordwagner <wagner.bradford@gmail.com>
2023-09-16 00:45:01 -0400

    it works

--- dots/config/nvim/lua/bradfordwagner/keybindings/ag.lua
+++ dots/config/nvim/lua/bradfordwagner/keybindings/ag.lua
@@ -7,10 +7,19 @@ return quick_telescope.setup {
   name = 'ag',
   picker_options = {
     ['live search'] = 'Ag<cr>',
-    ['input'] = function() 
+    ['input'] = function()
       vim.ui.input({ prompt = 'Ag> ' }, function (input)
         if input ~= nil then vim.cmd(string.format('Ag %s', input)) end
       end)
+    end,
+    ['word'] = function()
+      local current_word = vim.call('expand', '<cword>')
+      if current_word ~= nil then vim.cmd(string.format('Ag %s', current_word)) end
+    end,
+    ['full word'] = function()
+      local current_word = vim.call('expand', '<cWORD>')
+      print(current_word)
+      if current_word ~= nil then vim.cmd(string.format('Ag %s', current_word)) end
     end,
   }
 }
