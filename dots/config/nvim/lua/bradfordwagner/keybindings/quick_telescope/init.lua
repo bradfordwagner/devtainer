01af5952d51c5756e03829d92a76da4b9755c96b
bradfordwagner <wagner.bradford@gmail.com>
2023-09-16 01:52:47 -0400

    cleanup

--- dots/config/nvim/lua/bradfordwagner/keybindings/quick_telescope/init.lua
+++ dots/config/nvim/lua/bradfordwagner/keybindings/quick_telescope/init.lua
@@ -1,7 +1,6 @@
 -- setup ag configuration
 
 -- imports
-local easypick = require 'easypick'
 local util = require 'bradfordwagner.util'
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
