441a6dd99d82b26b7895c1058f36ec19e561385f
bradfordwagner <wagner.bradford@gmail.com>
2023-09-16 00:20:16 -0400

    setup ag live search

--- dots/config/nvim/lua/bradfordwagner/keybindings/ag.lua
+++ dots/config/nvim/lua/bradfordwagner/keybindings/ag.lua
@@ -6,8 +6,11 @@ local quick_telescope = require 'bradfordwagner.keybindings.quick_telescope'
 return quick_telescope.setup {
   name = 'ag',
   picker_options = {
-    ["f1"] = function () print('a') end,
-    ["f2"] = function () print('b') end,
-    ["command"] = 'echo "hi friends"',
+    ['live search'] = 'Ag<cr>',
+    ['input'] = function() 
+      vim.ui.input({ prompt = 'Ag> ' }, function (input)
+        if input ~= nil then vim.cmd(string.format('Ag %s', input)) end
+      end)
+    end,
   }
 }
