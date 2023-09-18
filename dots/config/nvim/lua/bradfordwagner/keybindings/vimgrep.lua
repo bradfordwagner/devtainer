b7a33ab419a6eed8625d0f1e8aa8e8eae3eceeaf
bradfordwagner <wagner.bradford@gmail.com>
2023-09-17 23:09:19 -0400

    ported vimgrep

--- dots/config/nvim/lua/bradfordwagner/keybindings/vimgrep.lua
+++ dots/config/nvim/lua/bradfordwagner/keybindings/vimgrep.lua
@@ -22,53 +22,62 @@ end
 return quick_telescope.setup {
   name = 'vimgrep',
   picker_options = {
--- " vimgrep helpers
--- " current file
--- map <expr><silent> <Space>ggi ":vimgrep /" . input("grep current file: ") . "/ % \<CR>co"
--- map <silent> <Space>ggww :vimgrep /<c-r><c-w>/ % <cr>co
--- map <silent> <Space>ggwe :vimgrep /<c-r><c-a>/ % <cr>co
--- " all files
--- map <expr><silent> <Space>gai ":vimgrep /" . input("grep all files: ") . "/ **/* \<CR>co"
--- map <silent> <Space>gaww :vimgrep /<c-r><c-w>/ **/* <cr>co
--- map <silent> <Space>gawe :vimgrep /<c-r><c-a>/ **/* <cr>co
--- " dir matching
--- map <expr><silent> <Space>gd ":vimgrep /" . input("grep files in directory: ") . "/ **/**" . input("dir match: ") . "**/* \<CR>co"
     { 
-      name = 'current file',
+      name = 'file input',
       cmd = prompt(function (input)
         vim.cmd(string.format('vimgrep /%s/ %s', input, '%'))
         open_changelist()
       end)
     },
     { 
-      name = 'current file word',
+      name = 'file word',
       cmd = function ()
         local current_word = vim.call('expand', '<cword>')
         vim.cmd(string.format('vimgrep /%s/ %s', current_word, '%'))
         open_changelist()
       end
     },
+    { 
+      name = 'file full word',
+      cmd = function ()
+        local current_word = vim.call('expand', '<cWORD>')
+        vim.cmd(string.format('vimgrep /%s/ %s', current_word, '%'))
+        open_changelist()
+      end
+    },
+    { 
+      name = 'all input',
+      cmd = prompt(function (input)
+        vim.cmd(string.format('vimgrep /%s/ %s **/*', input, '%'))
+        open_changelist()
+      end)
+    },
+    { 
+      name = 'all word',
+      cmd = function ()
+        local current_word = vim.call('expand', '<cword>')
+        vim.cmd(string.format('vimgrep /%s/ %s **/*', current_word, '%'))
+        open_changelist()
+      end
+    },
+    { 
+      name = 'all full word',
+      cmd = function ()
+        local current_word = vim.call('expand', '<cWORD>')
+        vim.cmd(string.format('vimgrep /%s/ %s **/*', current_word, '%'))
+        open_changelist()
+      end
+    },
+    -- TODO
+    -- map <expr><silent> <Space>gd ":vimgrep /" . input("grep files in directory: ") . "/ **/**" . input("dir match: ") . "**/* \<CR>co"
     -- { 
-    --   name = 'input',
-    --   cmd = function()
-    --     vim.ui.input({ prompt = 'Ag> ' }, function (input)
-    --       if input ~= nil then vim.cmd(string.format('Ag %s', input)) end
+    --   name = 'all dir',
+    --   cmd = prompt(function (search)
+    --     prompt(function (directory)
+    --       vim.cmd(string.format('vimgrep /%s/ %s **/*%s**/*', search, '%', directory))
+    --       open_changelist()
     --     end)
-    --   end },
-    -- { 
-    --   name = 'word',
-    --   cmd = function()
-    --     local current_word = vim.call('expand', '<cword>')
-    --     if current_word ~= nil then vim.cmd(string.format('Ag %s', current_word)) end
-    --   end 
-    -- },
-    -- { 
-    --   name = 'full word',
-    --   cmd = function()
-    --     local current_word = vim.call('expand', '<cWORD>')
-    --     print(current_word)
-    --     if current_word ~= nil then vim.cmd(string.format('Ag %s', current_word)) end
-    --   end 
+    --   end)
     -- },
   }
 }
