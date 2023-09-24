ef2a9a1260cfdfa23c8b661b61f764675f821879
bradfordwagner <wagner.bradford@gmail.com>
2023-09-24 02:02:14 -0400

    migrate number

--- dots/config/nvim/lua/bradfordwagner/options.lua
+++ dots/config/nvim/lua/bradfordwagner/options.lua
@@ -10,10 +10,13 @@ vim.g.rooter_manual_only = 1
 -- lightspeed-disable-default-mappings
 -- must be loaded before lightspeed.vim
 vim.g.lightspeed_no_default_keymaps = 1
+
 -- end global ----------------------------------------------
 
 
 -- options -------------------------------------------------
 vim.o.scrolloff = 999 -- auto center cursor
 vim.o.mouse = '' -- disable mouse
+vim.o.number = true
+-- vim.o.relativenumber = true
 -- end options ---------------------------------------------
