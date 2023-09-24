0037f98c613e975cc1a81570e336b7005df2f682
bradfordwagner <wagner.bradford@gmail.com>
2023-09-24 01:41:24 -0400

    move options up

--- dots/config/nvim/lua/bradfordwagner/init.lua
+++ dots/config/nvim/lua/bradfordwagner/init.lua
@@ -1,10 +1,10 @@
 -- load plugin managers
 require 'bradfordwagner.packer'
+require 'bradfordwagner.options'
 
 -- must be before theme load
 require 'bradfordwagner.ui'
 require 'bradfordwagner.keybindings'
-require 'bradfordwagner.options'
 
 -- second state
 require 'bradfordwagner.theme'
