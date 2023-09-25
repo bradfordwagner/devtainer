8700d5b14c6cb9e3153ec2a3b12a5f13545d4e52
bradfordwagner <wagner.bradford@gmail.com>
2023-09-24 22:56:58 -0400

    add dap layout show repl on bottom

--- dots/config/nvim/lua/bradfordwagner/ui/dap.lua
+++ dots/config/nvim/lua/bradfordwagner/ui/dap.lua
@@ -1,5 +1,44 @@
 local dapui = require("dapui")
-dapui.setup {}
+dapui.setup {
+  layouts = {
+    {
+      elements = {
+        {
+          id = "console",
+          size = 0.05,
+        },
+        {
+          id = "scopes",
+          size = 0.25,
+        },
+        {
+          id = "breakpoints",
+          size = 0.25,
+        },
+        {
+          id = "stacks",
+          size = 0.25,
+        },
+        {
+          id = "watches",
+          size = 0.20,
+        },
+      },
+      position = "left",
+      size = 30,
+    },
+    {
+      elements = {
+        {
+          id = "repl",
+          size = 1.0,
+        },
+      },
+      position = "bottom",
+      size = 30,
+    },
+  },
+}
 
 local dapgo = require 'dap-go'
 dapgo.setup {
