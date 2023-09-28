bc2c781ef3fde9bef1a75096f0f2a2842ac8fd63
bradfordwagner <wagner.bradford@gmail.com>
2023-09-27 22:48:49 -0400

    gofmt on save

--- dots/config/nvim/lua/bradfordwagner/options.lua
+++ dots/config/nvim/lua/bradfordwagner/options.lua
@@ -20,3 +20,15 @@ vim.o.mouse = '' -- disable mouse
 vim.o.number = true
 -- vim.o.relativenumber = true
 -- end options ---------------------------------------------
+
+-- gofmt on save -------------------------------------------
+-- https://github.com/ray-x/go.nvim#format-on-save
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').gofmt()
+  end,
+  group = format_sync_grp,
+})
+-- end gofmt on save----------------------------------------
