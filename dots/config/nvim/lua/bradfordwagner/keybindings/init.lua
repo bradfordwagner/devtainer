36f5932af18bafa731f66cb3fc817bfcdb71212d
bradfordwagner <wagner.bradford@gmail.com>
2023-12-04 13:20:35 -0500

    prefix space

--- dots/config/nvim/lua/bradfordwagner/keybindings/init.lua
+++ dots/config/nvim/lua/bradfordwagner/keybindings/init.lua
@@ -166,7 +166,7 @@ augroup myGolang
       nmap <buffer> <LocalLeader>fs :GoFillStruct<cr>
       nmap <buffer> <LocalLeader>R :GoRename 
       " add := to the beggining of the line and go to insert mode
-      nmap <buffer> <LocalLeader>= I := <Esc>I
+      nmap <buffer> <LocalLeader>= I  := <Esc>I
     endfunction
     autocmd FileType go call s:my_go_bindings()
 augroup END
