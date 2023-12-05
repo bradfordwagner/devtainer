c317a1edad29ff59fb2e4ba306802ecddfcc2832
bradfordwagner <wagner.bradford@gmail.com>
2023-12-05 08:03:21 -0500

    config timing

--- dots/config/nvim/lua/bradfordwagner/ui/mini.lua
+++ dots/config/nvim/lua/bradfordwagner/ui/mini.lua
@@ -1,2 +1,21 @@
 -- https://github.com/echasnovski/mini.nvim
-require('mini.animate').setup() -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-animate.md
+-- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-animate.md
+local animate = require('mini.animate')
+local timing = animate.gen_timing.linear({duration = 150, unit = 'total'})
+animate.setup({
+    cursor = {
+      timing = timing,
+    },
+    scroll = {
+      timing = timing,
+    },
+    resize = {
+      timing = timing,
+    },
+    open = {
+      timing = timing,
+    },
+    close = {
+      timing = timing,
+    },
+})
