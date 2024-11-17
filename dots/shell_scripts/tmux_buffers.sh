169369ea2635eda1b7dbb9b10774d0cd52b81288
bradfordwagner <wagner.bradford@gmail.com>
2024-11-17 12:34:00 -0500

    use preview win

--- dots/shell_scripts/tmux_buffers.sh
+++ dots/shell_scripts/tmux_buffers.sh
@@ -10,6 +10,7 @@ buffer=$(eval ${command} 2>/dev/null \
       --exit-0 \
       --delimiter : \
       --preview "echo {2} | bat -f --style=full -l bash" \
+      --preview-window=top:50% \
       --bind 'enter:become:echo {1}' \
       --bind "ctrl-d:execute-silent(tmux deleteb -b {1})+reload:${command}" \
 )
