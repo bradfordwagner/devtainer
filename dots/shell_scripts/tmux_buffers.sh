37668adb7c90d467d3fcf5262ee704ec102c37ac
bradfordwagner <wagner.bradford@gmail.com>
2024-11-17 12:19:10 -0500

    allow pasting into popup

--- dots/shell_scripts/tmux_buffers.sh
+++ dots/shell_scripts/tmux_buffers.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 set -e
 
+mode=$1
+
 # ctrl-d to delete buffer and refresh the buffer list
 command='tmux list-buffers -F "#{buffer_name}:#{buffer_sample}"'
 buffer=$(eval ${command} 2>/dev/null \
@@ -11,4 +13,13 @@ buffer=$(eval ${command} 2>/dev/null \
       --bind 'enter:become:echo {1}' \
       --bind "ctrl-d:execute-silent(tmux deleteb -b {1})+reload:${command}" \
 )
-[ "${buffer}" != "" ] && tmux paste-buffer -b ${buffer}
+
+if [ "${buffer}" != "" ]; then
+  if [ "${mode}" = "popup" ]; then
+    buffer_contents=$(tmux show-buffer -b ${buffer})
+    set -x
+    eval ${buffer_contents}
+  elif [ "${mode}" = "paste" ]; then
+    tmux paste-buffer -b ${buffer}
+  fi
+fi
