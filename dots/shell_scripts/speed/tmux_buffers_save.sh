f30967018987c6287ca3c4b6970b474a2221369f
bradfordwagner <wagner.bradford@gmail.com>
2024-11-19 13:01:39 -0500

    make it more understandable

--- dots/shell_scripts/speed/tmux_buffers_save.sh
+++ dots/shell_scripts/speed/tmux_buffers_save.sh
@@ -9,9 +9,11 @@ buffer_names=$(tmux list-buffers -F "#{buffer_name}")
 mkdir ~/.tmux 2> /dev/null
 buffer_file=~/.tmux/buffers.txt
 echo -n "" > ${buffer_file}
-for buffer in $(echo ${buffer_names}); do
-  encoded_buffer=$(tmux show-buffer -b ${buffer} | base64)
-  echo "${buffer} ${encoded_buffer}" >> ${buffer_file}
+for buffer_name in $(echo ${buffer_names}); do
+  buffer_value=$(tmux show-buffer -b ${buffer_name})
+  encoded_buffer_value=$(echo ${buffer_value} | base64)
+  echo ${palette_lpurple}${buffer_name} ${palette_cyan}${buffer_value}${palette_restore}
+  echo "${buffer_name} ${encoded_buffer_value}" >> ${buffer_file}
 done
 
 # echo buffer file:
