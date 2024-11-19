77b22a771bb8b91782b25837ae6d529c4069f6de
bradfordwagner <wagner.bradford@gmail.com>
2024-11-19 14:02:22 -0500

    trial no "-E" for fzf speed

--- dots/shell_scripts/speed/tmux_buffers_edit.sh
+++ dots/shell_scripts/speed/tmux_buffers_edit.sh
@@ -6,3 +6,6 @@ mkdir ~/.tmux 2> /dev/null
 buffer_file=~/.tmux/buffers.txt
 
 ${EDITOR} ${buffer_file}
+
+# load buffers after making changes
+~/.dotfiles/dots/shell_scripts/speed/tmux_buffers_load.sh
