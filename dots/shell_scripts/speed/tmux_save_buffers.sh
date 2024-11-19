1e81c7a056a0eafb69fd74495f6e8ac1f2227123
bradfordwagner <wagner.bradford@gmail.com>
2024-11-19 12:44:04 -0500

    cleanup

--- dots/shell_scripts/speed/tmux_save_buffers.sh
+++ dots/shell_scripts/speed/tmux_save_buffers.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 # set -e
 
-echo saving buffers
+echo ${palette_red}Saving Buffers${palette_restore}
 
 buffer_names=$(tmux list-buffers -F "#{buffer_name}")
 
+# mkdir tmux with silent error
 mkdir ~/.tmux 2> /dev/null
 buffer_file=~/.tmux/buffers.txt
 echo -n "" > ${buffer_file}
@@ -14,7 +15,7 @@ for buffer in $(echo ${buffer_names}); do
 done
 
 # echo buffer file:
-bat -P ${buffer_file}
+bat -P ${buffer_file} -lproperties
 
 # wait for input
 echo -n "Press enter to continue"
