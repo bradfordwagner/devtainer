fd1ae6fbd0c4c3a077a2dda50aafca589dff7aa7
bradfordwagner <wagner.bradford@gmail.com>
2025-04-05 22:05:38 -0400

    update

--- dots/shell_scripts/rofi_apps.sh
+++ dots/shell_scripts/rofi_apps.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
+# theme=~/.config/rofi/launchers/type-3/style-3.rasi
+# theme=~/.config/rofi/launchers/type-1/style-1.rasi
+theme=~/.config/rofi/launchers/type-2/style-1.rasi
 open -a Xquartz
 (ls -d /Applications/*; ls -d ~/Applications/*; ls -d /System/Applications/*) \
-  | rofi -dmenu -i -p ">" \
+  | rofi -dmenu -i -p ">" -theme ${theme} -show-icons \
   | xargs -I {} open "{}"
--- dots/shell_scripts/rofi_screen_shots.sh
+++ dots/shell_scripts/rofi_screen_shots.sh
@@ -1,11 +1,10 @@
 #!/bin/zsh
 
 open -a Xquartz
-# (ls -d ${HOME}/screenshots/*) \
-#   | rofi -dmenu -i -show-icons -p ">" \
-#   | xargs -I {} open "{}"
 
-
-ls ~/screenshots/ | while read A ; do  echo -en "$A\x00icon\x1f~/screenshots/$A\n" ; done | rofi -dmenu 
-
+# doesnt work quite yet
+theme=~/.config/rofi/launchers/type-3/style-6.rasi
+ls ~/screenshots/ | while read A ; do  echo -en "$A\x00icon\x1f~/screenshots/$A\n" ; done \
+  | rofi -dmenu -i -p ">" -theme ${theme} -show-icons \
+  | xargs -I {} pbcopy < "~/screenshots/{}"
 
