cc5b8c86b6efc42a54cedf3720ea0db681b3485a
bradfordwagner <wagner.bradford@gmail.com>
2025-04-05 22:31:56 -0400

    dmenu style

--- dots/shell_scripts/rofi_apps.sh
+++ dots/shell_scripts/rofi_apps.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # theme=~/.config/rofi/launchers/type-3/style-3.rasi
-theme=~/.config/rofi/launchers/type-1/style-1.rasi
+theme=~/.config/rofi/launchers/type-1/style-10.rasi
 # theme=~/.config/rofi/launchers/type-2/style-7.rasi
 open -a Xquartz
 (cd /Applications && ls -d *.app; cd ~/Applications/ && ls -d *.app; cd /System/Applications/ && ls -d *.app) \
