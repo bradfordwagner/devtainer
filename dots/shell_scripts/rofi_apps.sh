2a92668404e9d3541fc2c84a44589f5df94300d9
bradfordwagner <wagner.bradford@gmail.com>
2025-04-05 21:11:29 -0400

    cleanup

--- dots/shell_scripts/rofi_apps.sh
+++ dots/shell_scripts/rofi_apps.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-open -a "Xquartz"
+open -a Xquartz
 (ls -d /Applications/*; ls -d ~/Applications/*; ls -d /System/Applications/*) \
   | rofi -dmenu -i -p ">" \
   | xargs -I {} open "{}"
