ec07a878cd3243616478ae3d51694bc563262c8f
bradfordwagner <wagner.bradford@gmail.com>
2025-04-05 23:49:42 -0400

    cleanup

--- dots/shell_scripts/rofi_apps.sh
+++ dots/shell_scripts/rofi_apps.sh
@@ -1,8 +1,6 @@
 #!/bin/zsh
 
-# theme=~/.config/rofi/launchers/type-3/style-3.rasi
 theme=~/.config/rofi/launchers/type-1/style-10.rasi
-# theme=~/.config/rofi/launchers/type-2/style-7.rasi
 open -a Xquartz
 (cd /Applications && ls -d *.app; cd ~/Applications/ && ls -d *.app; cd /System/Applications/ && ls -d *.app) \
   | sed 's/\.app$//' \
