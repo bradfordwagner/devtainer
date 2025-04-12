372f418ba780343a52b77a9b7d72537f6c912e37
bradfordwagner <wagner.bradford@gmail.com>
2025-04-11 21:20:29 -0400

    rename prompt

--- dots/shell_scripts/rofi_apps.sh
+++ dots/shell_scripts/rofi_apps.sh
@@ -2,8 +2,9 @@
 
 theme=~/.config/rofi/launchers/type-1/style-10.rasi # dmenu
 theme=~/.config/rofi/launchers/type-4/style-2.rasi  # centered launcher
+prompt='app>'
 open -a Xquartz
 (cd /Applications && ls -d *.app; cd ~/Applications/ && ls -d *.app; cd /System/Applications/ && ls -d *.app) \
   | sed 's/\.app$//' \
-  | rofi -dmenu -i -p ">" -theme ${theme} -show-icons \
+  | rofi -dmenu -i -theme ${theme} -show-icons -p ${prompt} \
   | xargs -I {} open -a "{}"
