ebdd24aafe5241396c0776f2e24ddc46b0c01a5a
bradfordwagner <wagner.bradford@gmail.com>
2025-11-20 10:17:37 -0500

    refactor(rofi_jdi): generalize prompt jump_idea->jump_editor, ih->ch

--- dots/shell_scripts/rofi_jdi.sh
+++ dots/shell_scripts/rofi_jdi.sh
@@ -3,8 +3,8 @@
 theme=~/.config/rofi/launchers/type-1/style-10.rasi # dmenu
 theme=~/.config/rofi/launchers/type-4/style-10.rasi  # dmenu round
 theme=~/.config/rofi/launchers/type-4/style-1.rasi  # centered launcher
-prompt='jump_idea>'
+prompt='jump_editor>'
 open -a Xquartz
 (jdl) \
   | rofi -dmenu -i -theme ${theme} -p ${prompt} \
-  | xargs -I {} zsh -lc 'jumpdir addweight "{}" && cd {} && ih'
+  | xargs -I {} zsh -lc 'jumpdir addweight "{}" && cd {} && ch'
