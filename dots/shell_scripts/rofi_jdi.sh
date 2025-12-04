eca083a8becd5a71d0aa48dac113cfb5d5d37e80
bradfordwagner <wagner.bradford@gmail.com>
2025-12-04 16:28:19 -0500

    switch to idea

--- dots/shell_scripts/rofi_jdi.sh
+++ dots/shell_scripts/rofi_jdi.sh
@@ -4,7 +4,8 @@ theme=~/.config/rofi/launchers/type-1/style-10.rasi # dmenu
 theme=~/.config/rofi/launchers/type-4/style-10.rasi  # dmenu round
 theme=~/.config/rofi/launchers/type-4/style-1.rasi  # centered launcher
 prompt='jump_editor>'
+editor_cmd=ih
 open -a Xquartz
 (jdl) \
   | rofi -dmenu -i -theme ${theme} -p ${prompt} \
-  | xargs -I {} zsh -lc 'jumpdir addweight "{}" && cd {} && ch'
+  | xargs -I {} zsh -lc "jumpdir addweight '{}' && cd {} && ${editor_cmd}"
