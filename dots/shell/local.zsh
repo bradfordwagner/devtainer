839c7e3da4d467c29f9c1367b2da4053fb71a810
bradfordwagner <wagner.bradford@gmail.com>
2026-01-26 10:48:23 -0500

    feat(alias): mac_kill_all_apps

--- dots/shell/local.zsh
+++ dots/shell/local.zsh
@@ -5,5 +5,22 @@ function setBackground() {
     local backgroundPath=$(realpath $1)
     osascript -e 'tell application "System Events" to tell every desktop to set picture to "'${backgroundPath}'"'
 }
+function mac_kill_all_apps() {
+  osascript -e '-- get list of open apps
+tell application "System Events"
+  set allApps to displayed name of (every process whose background only is false) as list
+end tell
+
+-- leave some apps open
+set exclusions to {"Finder", "LaunchBar"}
+
+-- quit each app
+repeat with thisApp in allApps
+  set thisApp to thisApp as text
+  if thisApp is not in exclusions then
+    tell application thisApp to quit
+  end if
+end repeat'
+}
 ################################################
 
