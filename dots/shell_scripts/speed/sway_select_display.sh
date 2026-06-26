8f2403672a7f6fdc1ec8dad98e23d7da411bf6b8
bradfordwagner <wagner.bradford@gmail.com>
2026-06-26 11:47:45 -0400

    feat(sway): fzf display picker in speed menu, static 34" default output

---
+++ dots/shell_scripts/speed/sway_select_display.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+choice=$(printf '34" screen\nlaptop' | fzf --prompt="Display: " --height=~10)
+
+case "$choice" in
+  '34" screen') swaymsg "output X11-1 resolution 3440x1440 position 0 0 scale 1.0" ;;
+  'laptop')     swaymsg "output X11-1 resolution 2496x1664 position 0 0 scale 1.4" ;;
+esac
--- dots/shell_scripts/sway_display_setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-swaymsg "output X11-1 resolution 3440x1440 position 0 0 scale 1.0"
