2a20b74f1e2139408debd983437125d692e997a9
bradfordwagner <wagner.bradford@gmail.com>
2026-06-26 11:42:01 -0400

    fix(sway): force 3440x1440 and fix position syntax for swaymsg

--- dots/shell_scripts/sway_display_setup.sh
+++ dots/shell_scripts/sway_display_setup.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-res=$(swaymsg -t get_outputs -r | jq -r '.[] | select(.name == "X11-1") | "\(.current_mode.width)x\(.current_mode.height)"')
-
-case "$res" in
-  3440x1440) swaymsg "output X11-1 scale 1.0 position 0,0" ;;
-  *)         swaymsg "output X11-1 scale 1.4 position 0,0" ;;
-esac
+swaymsg "output X11-1 resolution 3440x1440 position 0 0 scale 1.0"
