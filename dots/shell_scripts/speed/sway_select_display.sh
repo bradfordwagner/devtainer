63032d99f493a7904205587ad9274d5d813dd1b9
bradfordwagner <wagner.bradford@gmail.com>
2026-06-28 17:14:09 -0400

    sway laptop scale 1.4->1.2

--- dots/shell_scripts/speed/sway_select_display.sh
+++ dots/shell_scripts/speed/sway_select_display.sh
@@ -3,5 +3,5 @@ choice=$(printf '34" screen\nlaptop' | fzf --prompt="Display: " --height=~10)
 
 case "$choice" in
   '34" screen') swaymsg "output X11-1 resolution 3440x1440 position 0 0 scale 1.0" ;;
-  'laptop')     swaymsg "output X11-1 resolution 2496x1664 position 0 0 scale 1.4" ;;
+  'laptop')     swaymsg "output X11-1 resolution 2496x1664 position 0 0 scale 1.2" ;;
 esac
