43a710da34bd68adbb8153ede5e77ae6e041e0f8
bradfordwagner <wagner.bradford@gmail.com>
2026-07-09 16:12:03 -0400

    feat(sway): add small/medium/large scale options for laptop display

--- dots/shell_scripts/speed/sway_select_display.sh
+++ dots/shell_scripts/speed/sway_select_display.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-choice=$(printf '34" screen\nlaptop' | fzf --prompt="Display: " --height=~10)
+choice=$(printf '34" screen\nlaptop small\nlaptop medium\nlaptop large' | fzf --prompt="Display: " --height=~10)
 
 case "$choice" in
-  '34" screen') swaymsg "output X11-1 resolution 3440x1440 position 0 0 scale 1.0" ;;
-  'laptop')     swaymsg "output X11-1 resolution 2496x1664 position 0 0 scale 1.2" ;;
+  '34" screen')    swaymsg "output X11-1 resolution 3440x1440 position 0 0 scale 1.0" ;;
+  'laptop small')  swaymsg "output X11-1 resolution 2496x1664 position 0 0 scale 1.2" ;;
+  'laptop medium') swaymsg "output X11-1 resolution 2496x1664 position 0 0 scale 1.4" ;;
+  'laptop large')  swaymsg "output X11-1 resolution 2496x1664 position 0 0 scale 1.6" ;;
 esac
